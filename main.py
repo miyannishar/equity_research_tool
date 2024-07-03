e1b4263f604eaaf577779d9a2ba42c6460c0f33e
miyannishar <miyannishar786@gmail.com>
2024-07-02 21:55:16 -0400

    af

--- main.py
+++ main.py
@@ -10,10 +10,9 @@ from langchain.document_loaders import UnstructuredURLLoader
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 
-from dotenv import load_dotenv
 import os
 
-load_dotenv()
+
 
 os.environ['OPENAI_API_KEY']=os.getenv("OPENAI_API_KEY")
 
